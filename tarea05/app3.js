4262ceeeb5a53a6dd4097df680979c9af5605aa8
andres_garcia <andygarciac5@gmail.com>
2023-09-11 00:12:36 -0600

    se agregara la tarea05

---
+++ tarea05/app3.js
@@ -0,0 +1,21 @@
+var nombre="whatever you say";
+var nombre="whatever I say";
+console.log(nombre);
+
+
+
+// Comentarios
+
+let user="Uron";
+
+
+
+console.log(user);
+
+let numero1=5;
+let numero2=3;
+
+console.log(numero1+numero2);
+console.log(numero1*numero2);
+console.log(numero1/numero2);
+console.log(numero1-numero2);
---
+++ tarea05/app4.js
@@ -0,0 +1,14 @@
+//identificacion
+let nombreUser="Andres";
+let edad=5;
+let nacionalidad="Mexicana";
+let comprometido="NO";
+
+console.log("Nombre es: ");
+console.log(nombreUser);
+console.log("Edad es: ");
+console.log(edad);
+console.log("Su nacionalidad es: ");
+console.log(nacionalidad);
+console.log("Estas comprometido? ");
+console.log(comprometido);
